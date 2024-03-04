772f6a455ed8b839de705960852cc7ecb0e05e42
sunyan <sunyan@joygames.cc>
2024-03-04 17:26:27 +0800

    feat: 我的

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,6 +1,7 @@
-// src/pages/my/my.vue
-
 <script setup lang="ts">
+import { useMemberStore } from '@/stores'
+import { onLoad } from '@dcloudio/uni-app'
+import { useGuessList } from '@/composables'
 // 获取屏幕边界到安全区域距离
 const { safeAreaInsets } = uni.getSystemInfoSync()
 // 订单选项
@@ -10,23 +11,23 @@ const orderTypes = [
   { type: 3, text: '待收货', icon: 'icon-check' },
   { type: 4, text: '待评价', icon: 'icon-comment' },
 ]
+const memberStore = useMemberStore()
+const { guessRef, onScrolltolower } = useGuessList()
 </script>
 
 <template>
-  <scroll-view class="viewport" scroll-y enable-back-to-top>
+  <scroll-view class="viewport" scroll-y enable-back-to-top @scrolltolower="onScrolltolower">
     <!-- 个人资料 -->
     <view class="profile" :style="{ paddingTop: safeAreaInsets!.top + 'px' }">
       <!-- 情况1：已登录 -->
-      <view class="overview" v-if="false">
+      <view class="overview" v-if="memberStore.profile">
         <navigator url="/pagesMember/profile/profile" hover-class="none">
-          <image
-            class="avatar"
-            mode="aspectFill"
-            src="https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/avatar_3.jpg"
-          ></image>
+          <image class="avatar" mode="aspectFill" :src="memberStore.profile.avatar"></image>
         </navigator>
         <view class="meta">
-          <view class="nickname"> 黑马程序员 </view>
+          <view class="nickname">
+            {{ memberStore.profile.nickname || memberStore.profile.account }}
+          </view>
           <navigator class="extra" url="/pagesMember/profile/profile" hover-class="none">
             <text class="update">更新头像昵称</text>
           </navigator>
---
+++ src/pagesMember/pagesMember.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="pagesMember">pagesMember</div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped></style>
